eca7c62355f71ea16147b71a2530562f0556221d
Marcin Tyborowski <gunshino@gmail.com>
2018-10-19 17:29:22 +0200

    DI configuration and examples

---
+++ BialHackApi.Base/Services/ExampleService.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Configuration;
+using System.Threading.Tasks;
+
+namespace BialHackApi.Base.Services
+{
+    public class ExampleService
+    {
+        private readonly IConfiguration configuration;
+        private readonly IHttpContextAccessor httpContextAccessor;
+
+        public ExampleService(IConfiguration configuration, IHttpContextAccessor httpContextAccessor)
+        {
+            this.configuration = configuration;
+            this.httpContextAccessor = httpContextAccessor;
+        }
+
+        public async Task<object> ExampleMethod()
+        {
+            var setting = configuration["Example:Setting"];
+            var httpContext = httpContextAccessor.HttpContext;
+
+            return new object();
+        }
+    }
+}
--- BialHackApi.Web/Startup.cs
+++ BialHackApi.Web/Startup.cs
@@ -1,21 +1,20 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Builder;
+﻿using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Logging;
-using Microsoft.Extensions.Options;
 
 namespace BialHackApi.Web
 {
     public class Startup
     {
-        public Startup(IConfiguration configuration)
+        public Startup(IHostingEnvironment env)
         {
-            Configuration = configuration;
+            var builder = new ConfigurationBuilder()
+                 .SetBasePath(env.ContentRootPath)
+                 .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
+                 .AddJsonFile($"appsettings.{env.EnvironmentName}.json", optional: true)
+                 .AddEnvironmentVariables();
+            Configuration = builder.Build();
         }
 
         public IConfiguration Configuration { get; }
@@ -23,7 +22,15 @@ namespace BialHackApi.Web
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddCors(options =>
+            {
+                options.AddPolicy("CorsPolicy",
+                   builder => builder.AllowAnyOrigin()
+                       .AllowAnyMethod()
+                       .AllowAnyHeader());
+            });
             services.AddMvc();
+
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -31,9 +38,17 @@ namespace BialHackApi.Web
         {
             if (env.IsDevelopment())
             {
+                app.UseBrowserLink();
                 app.UseDeveloperExceptionPage();
             }
+            else
+            {
+                app.UseExceptionHandler("/Home/Error");
+            }
 
+            app.UseStaticFiles();
+
+            app.UseCors("CorsPolicy");
             app.UseMvc();
         }
     }
