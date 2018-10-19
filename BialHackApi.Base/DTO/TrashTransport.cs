db278bbdc60fedba227d1a4dc9b0b484ebe809fd
Marcin Tyborowski <gunshino@gmail.com>
2018-10-19 20:51:07 +0200

    setup excel library

---
+++ BialHackApi.Base/DTO/TrashTransport.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace BialHackApi.Base.DTO
+{
+    public class TrashTransport
+    {
+        public long Id { get; set; }
+        public DateTime Date { get; set; }
+        public string Description { get; set; }
+        public string RfId0 {get;set;}
+        public string VehicleName { get; set; }
+        public string VehicleNumber { get; set; }
+        public string TrashType { get; set; }
+        public string Container { get; set; }
+        public string Note { get; set; }
+        public string MgoType { get; set; }
+        public double Latitude { get; set; }
+        public double Longitude { get; set; }
+    }
+}
