fd421300f18333d5cd4c002fd4ec730f52cb093c
Marcin Tyborowski <gunshino@gmail.com>
2018-10-19 18:34:29 +0200

    added google maps service

---
+++ BialHackApi.Base/DTO/GoogleMapsDTO/GoogleMapsDistanceDurationDTO.cs
@@ -0,0 +1,10 @@
+﻿using BialHackApi.Base.Interfaces;
+
+namespace BialHackApi.Base.DTO
+{
+    public class GoogleMapsDistanceDurationDTO : IDistanceDuration
+    {
+        public int Distance { get; set; }
+        public int Duration { get; set; }
+    }
+}
---
+++ BialHackApi.Base/DTO/GoogleMapsDTO/GoogleMapsDistanceResponseDTO.cs
@@ -0,0 +1,37 @@
+﻿using Newtonsoft.Json;
+
+namespace BialHackApi.Base.DTO
+{
+    public class GoogleMapsDistanceResponseDTO
+    {
+        [JsonProperty("origin_addresses")]
+        public string[] StartLocation { get; set; }
+
+        [JsonProperty("destination_addresses")]
+        public string[] TargetLocation { get; set; }
+
+        [JsonProperty("rows")]
+        public RowsDTO[] Rows { get; set; }
+    }
+    public class RowDTO
+    {
+        [JsonProperty("distance")]
+        public TextValueDTO Distance { get; set; }
+        [JsonProperty("duration")]
+        public TextValueDTO Duration { get; set; }
+    }
+
+    public class TextValueDTO
+    {
+        [JsonProperty("text")]
+        public string Text { get; set; }
+        [JsonProperty("value")]
+        public int Value { get; set; }
+    }
+
+    public class RowsDTO
+    {
+        [JsonProperty("elements")]
+        public RowDTO[] Elements { get; set; }
+    }
+}
